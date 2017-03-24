50b8f3025827cd338a8e9f6a088cfe16a21e377d
Eric Andres <emandres@gmail.com>
2017-03-24 15:32:57 -0600

    Better function.js

--- function.js
+++ function.js
@@ -1,5 +1,17 @@
 const child_process = require('child_process')
 
-exports.handle = () => {
-    //child_process.spawn("target/")
+exports.handle = (event, context, callback) => {
+    try {
+        child_process.execSync('mv ./slackbot /tmp; chmod +x /tmp/slackbot')
+        var child = child_process.exec('/tmp/slackbot', (err, stdout, stderr) => {
+            if (err) {
+                callback("Error: " + err)
+            }
+            callback(null, stdout.toString('utf8'))
+        })
+        child.stdin.write(JSON.stringify(event))
+        child.stdin.end()
+    } catch (e) {
+        callback("Catch: " + e.stderr.toString('utf8'))
+    }
 }
